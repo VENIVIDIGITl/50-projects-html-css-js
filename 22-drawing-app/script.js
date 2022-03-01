cacd20fc44db6c7927fd8ddc251ec44a995c9517
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-03-01 14:44:36 +0200

    add 22nd project

---
+++ 22-drawing-app/script.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById('canvas');
+const increaseBtn = document.getElementById('increase');
+const decreaseBtn = document.getElementById('decrease');
+const sizeEL = document.getElementById('size');
+const colorEl = document.getElementById('color');
+const clearEl = document.getElementById('clear');
+
+const ctx = canvas.getContext('2d');
+
+let x;
+let y;
+let size = 10;
+colorEl.value = 'black';
+let color = colorEl.value;
+let isPressed = false;
+
+
+canvas.addEventListener('mousedown', event => {
+  isPressed = true;
+  x = event.offsetX;
+  y = event.offsetY;
+});
+
+
+canvas.addEventListener('mouseup', event => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+
+
+canvas.addEventListener('mousemove', event => {
+  if(isPressed) {
+    const x2 = event.offsetX;
+    const y2 = event.offsetY;
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  };
+});
+
+
+colorEl.addEventListener('change', event => color = event.target.value);
+
+clearEl.addEventListener('click', () => ctx.clearRect(0,0, canvas.width, canvas.height));
+
+increaseBtn.addEventListener('click', () => {
+  size += 5;
+  if(size === 6) size = 5;
+  if(size >= 50) size = 50;
+  updateSizeOnScreen()
+});
+
+decreaseBtn.addEventListener('click', () => {
+  size -= 5;
+  if(size <= 0) size = 1;
+  updateSizeOnScreen()
+});
+
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+};
+
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
+};
+
+
+function updateSizeOnScreen() {
+  sizeEL.innerText = size;
+};
